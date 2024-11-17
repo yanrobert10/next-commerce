85506a86d326804adf7d65308c0ab8cdd8a72e7e
yanrobert10 <yan39robert@gmail.com>
2024-11-17 10:33:08 -0300

    Listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="max-w-7-xl max-auto pt-8 px-8 xl:px-0">
+        <div className=" 
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          gap-10
+          xl:gap-6"
+        >
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            Prod 1
+          </div>
+
+          
+       </div>
     </div>
   )
 }
